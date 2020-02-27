b78c2c9e6b2d508c6ca444d76f46579794aabc8b
DavidKoen <51445963+DavidKoen@users.noreply.github.com>
2020-02-27 14:27:45 +0200

    Finished the app.

--- app.js
+++ app.js
@@ -1,30 +1,80 @@
-let qwerty = document.querySelector('#qwerty'); // Keyboard rows.
-let list = document.querySelector('#phrase'); // The game word.
-let missed = 0; // Chances still to guess. 5 is game over.
-let start = document.querySelector('.btn__reset'); // The start up button.
-let overlay = document.querySelector('#overlay'); // The start up screen.
-const phrases = ['right', 'hello', 'floor', 'space', 'night']; // The word options.
-const phraseArray = getRandomPhraseAsArray(phrases); // The game word.
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startButton = document.querySelector('.btn__reset');
+let missed = 0;
+let overlay = document.getElementById('overlay');
+const phrases = ['hello', 'goodbye', 'lost', 'found', 'alive'];
+const title= document.querySelector('.title');
 
-// Start up game.
-start.addEventListener('click', () => {
+// Starts the game.
+startButton.addEventListener('click', () => {
     overlay.style.display = 'none';
 });
 
-// Collect the game word.
+// Chooses the game word.
 function getRandomPhraseAsArray(arr) {
-    let characters = arr[Math.floor(Math.random() * phrases.length)].split('');
-    return characters;
+     let arrayObject = arr[Math.floor(Math.random() * arr.length)];
+     let randomPhrase = arrayObject;
+     return randomPhrase.split('');
+}
+
+
+// Add letters to the display.
+function addPhraseToDisplay(arr) {
+    for (let i = 0; i < arr.length; i += 1) {
+        let ul = document.querySelector('#phrase ul');
+        let li = document.createElement('LI');
+        li.textContent = arr[i];
+        if (li.textContent !== ' ') {
+            li.className = 'letter';
+        } 
+       phraseToDisplay = ul.appendChild(li);
+    }  return phraseToDisplay;
 };
 
-// Add the game word to the display.
-function addPhraseToDisplay(arr) {
-    for (let i = 0; i <= phraseArray.length; i += 1) {
-        let createList = document.createElement('LI');
-        let addText = document.createTextNode(phraseArray[i]);
-        createList.appendChild(addText);
-        let individualLetters = document.querySelector('#phrase ul').appendChild(createList);
-        individualLetters.className = 'letter';
-        return individualLetters;
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+// Check if the chosen button matches the game word letter.
+function checkLetter(button) {
+    let checkLetter = document.getElementsByClassName('letter');
+    let match = null;
+    for (let i = 0; i < checkLetter.length; i += 1) {
+        if (button.textContent === checkLetter[i].textContent.toLowerCase()) {
+            checkLetter[i].classList.add('show');
+            match = button.textContent;
+        }
+    } return match
+};
+
+// Check the buttons the player selected.
+qwerty.addEventListener('click', e=> {
+    if (e.target.tagName == 'BUTTON') {
+        e.target.className = 'chosen'
+        let chosen = document.querySelectorAll('.chosen');
+        chosen.disabled = true;
+        let letterFound = checkLetter(e.target);
+        if (letterFound === null) {
+            let ol = document.querySelector('#scoreboard ol');
+            let li = document.querySelector('#scoreboard li')
+            let removeHeart = ol.removeChild(li);
+            missed += 1;
+        }
+    } 
+    checkWin(e.target);
+    });
+
+// Check if player won or lost.
+ function checkWin() {
+    let show = document.getElementsByClassName('show');
+    let letter = document.getElementsByClassName('letter');
+    if (show.length === letter.length) {
+        overlay.classList.add('win');
+        title.textContent = 'ongratulations! You won!';
+        overlay.style.display = 'flex';
+    } else if (missed >= 5) {
+        overlay.classList.add('lose');
+        title.textContent = 'Sorry, better luck next time.';
+        overlay.style.display = 'flex';
     }
 }
